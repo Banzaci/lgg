21e722abe5178f3842a900d41143ddf3c0c4db79
Michael Larsson <michael.larsson@tui.se>
2019-10-29 14:25:50 +0100

    Refactor name

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
 
-const SecondPage = () => (
+const About = () => (
   <Layout>
     <SEO title="About" />
   </Layout>
 )
 
-export default SecondPage
+export default About
